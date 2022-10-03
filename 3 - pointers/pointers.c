49f149aa64bbc9be7355f47046cfe4fd751f5e00
mledimna <mathieu-ledimna@outlook.com>
2022-10-03 10:59:56 +0200

    Change ptr

--- 3 - pointers/pointers.c
+++ 3 - pointers/pointers.c
@@ -4,8 +4,16 @@ int main(void){
     int my_integer = 42;
     int * int_ptr = NULL;
 
+    if (int_ptr == NULL) printf("ptr is NULL\r\n");
+    else printf("ptr is not NULL\r\n");
+
     printf("NULL ptr : 0x%p\r\n", int_ptr);
+    printf("Address ptr : 0x%p\r\n", &int_ptr);
+    
     int_ptr = &my_integer;
+
+    if (int_ptr == NULL) printf("ptr is NULL\r\n");
+    else printf("ptr is not NULL\r\n");
     printf("ptr = &my_integer : 0x%p\r\n", int_ptr);
 
     return 0;
